8b73a0b889fd6d3a8ca8ac25f038ebcf76c05378
matrix41 <raymond@vivaldi.ipac.caltech.edu>
2014-10-16 16:04:53 -0700

    added new code to insert the correct number of blank spaces in between data values and header fields

--- convert_header.pl
+++ convert_header.pl
@@ -5,6 +5,9 @@ use feature qw(switch say); # need this for GIVEN-WHEN block to work
 use Tie::IxHash;
 
 # Define
+my $s1;
+my $s2;
+my $s3;
 
 my @keywords;
 push(@keywords, "STAR_ID"); # 
@@ -58,11 +61,11 @@ for ( my $i = 0 ; $i <= $#array ; $i++ )
     }
     if ( $array[$i] =~ /#12#/ )
     {
-        print "\n\n ANDROMEDA 12 \n\n";
+#        print "\n\n ANDROMEDA 12 \n\n";
   }
     if ( $array[$i] =~ /#13#/ )
     {
-        print "\n\n ANDROMEDA 13 \n\n";
+#        print "\n\n ANDROMEDA 13 \n\n";
     }
     if ( $array[$i] =~ /#14#/ )
     {
@@ -108,10 +111,44 @@ for ( my $i = 0 ; $i <= $#array ; $i++ )
         if ( $array[$i] =~ /#22#/ )
         {
             $array[$i] =~ s/#22#/RELATIVE_MAGNITUDE/g;
+            for ( my $m = 0 ; $m <= $#array ; $m++ )
+            {
+                if ( $array[$m] =~ /\|\s\s\s\s\sreal\s\|/ )
+                {
+                    $s1 .= (" " x ( (length("RELATIVE_MAGNITUDE") ) ) );
+                    $array[$m] =~ s/\|\s\s\s\s\sreal/\|$s1 real/g;
+                    $s1 = "";
+                }
+            }            
         }
         if ( $array[$i] =~ /#23#/ )
         {
             $array[$i] =~ s/#23#/MAGNITUDE_UNCERTAINTY/g;
+            for ( my $n = 0 ; $n <= $#array ; $n++ )
+            {
+                if ( $array[$n] =~ /\|\s\s\s\sreal\s\|/ )
+                {
+                    $s1 .= (" " x ( (length("MAGNITUDE_UNCERTAINTY") - 1 ) ) );
+                    $array[$n] =~ s/\|\s\s\s\sreal/\|$s1 real/g;
+                    $s1 = "";
+                }
+            }            
+        }
+        if ( $array[$i] =~ /\|\s+\|\s+\|\s+\|\s+\|/ )
+        {
+            my $s4 .= "";
+            my $s5 .= "";
+            my $s6 .= "";
+            my $s7 .= "";
+            $s4 .= (" " x (length("RELATIVE_MAGNITUDE") + 6 ) );
+            $s5 .= (" " x (length("MAGNITUDE_UNCERTAINTY") + 5 ) );
+            $s6 .= (" " x 12);
+            $s7 .= (" " x 11);
+            $array[$i] =~ s/\|\s+\|\s+\|\s+\|\s+\|/\|$s6\|$s4\|$s5\|$s7\|/g;
+            $s4 = undef;
+            $s5 = undef;
+            $s6 = undef;
+            $s7 = undef;
         }
         print     "$array[$i]";
         print $oh "$array[$i]";
@@ -120,9 +157,14 @@ for ( my $i = 0 ; $i <= $#array ; $i++ )
     if ( ( $array[$i] =~ /^\s+/ ) )
     {
         my @lightcurve = split /\s+/, $array[$i];
-        print     "$lightcurve[0]          $lightcurve[1]        $lightcurve[2]        $lightcurve[3]  $lightcurve[4]\n";
-        print $oh "$lightcurve[0]          $lightcurve[1]        $lightcurve[2]        $lightcurve[3]  $lightcurve[4]\n";
-
+        $s1 .= (" " x ( (length("RELATIVE_MAGNITUDE") - length("#22#") + 2 ) ) );
+        $s2 .= (" " x ( (length("MAGNITUDE_UNCERTAINTY") - length("#23#") + 2 ) ) );
+        $s3 .= (" " x ( 9 ) );
+        print     "  $lightcurve[1] $s1 $lightcurve[2] $s2 $lightcurve[3] $s3 $lightcurve[4]\n";
+        print $oh "  $lightcurve[1] $s1 $lightcurve[2] $s2 $lightcurve[3] $s3 $lightcurve[4]\n";
+        $s1 = "";
+        $s2 = "";
+        $s3 = "";
     }
 }
 
